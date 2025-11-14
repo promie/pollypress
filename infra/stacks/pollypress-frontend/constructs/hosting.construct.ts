2a6d183dc50b8c98170c23f4e47680f244235ea4
Promie Yutasane <promie.yutasane@gmail.com>
2025-11-15 07:59:29 +1100

    updated hosting construct to lookup hosted zone and domain setup

--- infra/stacks/pollypress-frontend/constructs/hosting.construct.ts
+++ infra/stacks/pollypress-frontend/constructs/hosting.construct.ts
@@ -4,18 +4,29 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
 
 export type FrontendHostingProps = {
     appName: string;
     apiUrl: string;
+    domainName?: string;
+    subdomain?: string;
+    certificate?: acm.ICertificate;
 };
 
 export class FrontendHostingConstruct extends Construct {
     public readonly distribution: cloudfront.Distribution;
     public readonly bucket: s3.Bucket;
+    public readonly customDomain?: string;
 
     constructor(scope: Construct, id: string, props: FrontendHostingProps) {
         super(scope, id);
+
+        const { domainName, subdomain, certificate } = props;
+        const fullDomain = domainName && subdomain ? `${subdomain}.${domainName}` : undefined;
+        this.customDomain = fullDomain;
 
         this.bucket = new s3.Bucket(this, 'FrontendBucket', {
             bucketName: `${props.appName.toLowerCase()}-frontend`,
@@ -25,8 +36,17 @@ export class FrontendHostingConstruct extends Construct {
             autoDeleteObjects: true,
         });
 
+        let hostedZone: route53.IHostedZone | undefined;
+        if (domainName && subdomain && fullDomain) {
+            hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
+                domainName: domainName,
+            });
+        }
+
         this.distribution = new cloudfront.Distribution(this, 'Distribution', {
             defaultRootObject: 'index.html',
+            domainNames: fullDomain ? [fullDomain] : undefined,
+            certificate: certificate,
             defaultBehavior: {
                 origin: origins.S3BucketOrigin.withOriginAccessControl(this.bucket),
                 viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
@@ -40,6 +60,16 @@ export class FrontendHostingConstruct extends Construct {
                 },
             ],
         });
+
+        if (hostedZone && fullDomain) {
+            new route53.ARecord(this, 'AliasRecord', {
+                zone: hostedZone,
+                recordName: subdomain,
+                target: route53.RecordTarget.fromAlias(
+                    new targets.CloudFrontTarget(this.distribution)
+                ),
+            });
+        }
 
         new s3deploy.BucketDeployment(this, 'DeployFrontend', {
             sources: [s3deploy.Source.asset('./frontend/dist')],
