918f650ad2d8c630046ed1632a14d0139e16ad72
Promie Yutasane <promie.yutasane@gmail.com>
2025-11-17 15:50:47 +1100

    construct the pollyPress event construct which has both receiver and processor

--- infra/stacks/pollypress-backend/stack.ts
+++ infra/stacks/pollypress-backend/stack.ts
@@ -1,6 +1,9 @@
 import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import { S3BucketsConstruct } from './constructs/storage/s3Buckets.construct';
+import { UploadApiConstruct } from './constructs/handlers/uploadApi.construct';
+import { PollyPressEventConstruct } from './constructs/events/pollyPressEvent.construct';
 
 export type PollyPressBackendStackProps = StackProps & {
     appName: string;
@@ -16,19 +19,49 @@ export class PollyPressBackendStack extends Stack {
     constructor(scope: Construct, id: string, props: PollyPressBackendStackProps) {
         super(scope, id, props);
 
-        const { domainName, subdomain } = props;
-        const customDomain = domainName && subdomain ? `${subdomain}.${domainName}` : undefined;
+        const { appName, stage, domainName, subdomain, certificate } = props;
 
-        this.apiUrl = customDomain ? `https://${customDomain}` : '';
+        const storage = new S3BucketsConstruct(this, 'Storage', {
+            appName,
+            stage,
+        });
 
-        if (this.apiUrl) {
-            new CfnOutput(this, 'ApiUrl', {
-                value: this.apiUrl,
-                description: 'API Gateway URL',
-            });
+        const api = new UploadApiConstruct(this, 'Api', {
+            appName,
+            stage,
+            inputBucket: storage.inputBucket,
+            domainName,
+            subdomain,
+            certificate,
+        });
 
+        new PollyPressEventConstruct(this, 'PollyPressEvent', {
+            appName,
+            stage,
+            inputBucket: storage.inputBucket,
+            outputBucket: storage.outputBucket,
+        });
+
+        this.apiUrl = api.apiUrl;
+
+        new CfnOutput(this, 'ApiUrl', {
+            value: this.apiUrl,
+            description: 'API Gateway URL',
+        });
+
+        new CfnOutput(this, 'InputBucket', {
+            value: storage.inputBucket.bucketName,
+            description: 'Input S3 Bucket',
+        });
+
+        new CfnOutput(this, 'OutputBucket', {
+            value: storage.outputBucket.bucketName,
+            description: 'Output S3 Bucket',
+        });
+
+        if (domainName && subdomain) {
             new CfnOutput(this, 'CustomDomain', {
-                value: customDomain!,
+                value: `${subdomain}.${domainName}`,
                 description: 'API Custom Domain',
             });
         }
